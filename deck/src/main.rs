4ed7d575c03d246a2efc69535fd934e8b4128759
Lukasz Malucha <lucasmalucha@gmail.com>
2025-05-14 18:06:08 +0100

    impl

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -4,25 +4,34 @@ struct Deck {
     cards: Vec<String>
 }
 
+impl Deck {
+    fn new() -> Self {
+        // List of 'suits' 
+        let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+        // List of 'values'
+        let values = ["Ace", "Two", "Three"];
+        // Double nested for loop
 
-fn main() {
-    // List of 'suits' 
-    let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
-    // List of 'values'
-    let values = ["Ace", "Two", "Three"];
-    // Double nested for loop
+        let mut cards = vec![];
 
-    let mut cards = vec![];
-
-    for suit in suits {
-        for value in values {
-            let card = format!("{} of {}", value, suit);
-            cards.push(card);    
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);    
+            }
         }
+
+        let deck = Deck { cards };
+
+        return deck;    
     }
 
-    let deck = Deck { cards };
+}
 
+
+fn main() {
+
+    let deck = Deck::new();
     println!("Deck: {:#?}", deck);
 }
 
