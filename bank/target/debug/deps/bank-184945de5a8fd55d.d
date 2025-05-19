cbd36c34511e8c3cfc4cd18bb8d241ac6cc8ec6b
Lukasz Malucha <lucasmalucha@gmail.com>
2025-05-19 18:13:54 +0100

    bank

---
+++ bank/target/debug/deps/bank-184945de5a8fd55d.d
@@ -0,0 +1,5 @@
+C:\Workshop\Rust Workshop VS\Rust-Workshop\bank\target\debug\deps\libbank-184945de5a8fd55d.rmeta: src\main.rs
+
+C:\Workshop\Rust Workshop VS\Rust-Workshop\bank\target\debug\deps\bank-184945de5a8fd55d.d: src\main.rs
+
+src\main.rs:
---
+++ bank/target/debug/deps/bank-649fc1d4ddd1b3fa.d
@@ -0,0 +1,5 @@
+C:\Workshop\Rust Workshop VS\Rust-Workshop\bank\target\debug\deps\libbank-649fc1d4ddd1b3fa.rmeta: src\main.rs
+
+C:\Workshop\Rust Workshop VS\Rust-Workshop\bank\target\debug\deps\bank-649fc1d4ddd1b3fa.d: src\main.rs
+
+src\main.rs:
