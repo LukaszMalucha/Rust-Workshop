b3ede620d4d2257f6728369cc51b22b428e7bc76
Lukasz Malucha <lucasmalucha@gmail.com>
2025-05-31 17:21:27 +0100

    media

---
+++ media/target/debug/deps/media.d
@@ -0,0 +1,5 @@
+C:\Workshop\Rust Workshop VS\Rust-Workshop\media\target\debug\deps\media.exe: src\main.rs
+
+C:\Workshop\Rust Workshop VS\Rust-Workshop\media\target\debug\deps\media.d: src\main.rs
+
+src\main.rs:
---
+++ media/target/debug/media.d
@@ -0,0 +1,1 @@
+C:\Workshop\Rust\ Workshop\ VS\Rust-Workshop\media\target\debug\media.exe: C:\Workshop\Rust\ Workshop\ VS\Rust-Workshop\media\src\main.rs
