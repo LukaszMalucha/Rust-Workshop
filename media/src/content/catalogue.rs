9028d81fc9100d69a963819e8cd80f24b65afc1e
Lukasz Malucha <lucasmalucha@gmail.com>
2025-06-15 17:42:24 +0100

    refactor

---
+++ media/src/content/catalogue.rs
@@ -0,0 +1,26 @@
+#[derive(Debug)]
+pub struct Catalogue {
+    items: Vec<Media>
+}
+
+impl Catalogue {
+    pub fn new() -> Self {
+        Catalogue { items: vec![] }
+    }
+
+    pub fn add(&mut self, media: Media) {
+        self.items.push(media);
+
+    }
+
+    pub fn get_by_index(&self, index: usize) -> Option<&Media> {
+        if self.items.len() > index {
+            Some(&self.items[index])
+        } else {
+            None    
+        }
+        
+
+    }
+
+}
