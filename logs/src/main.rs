0157ba5302be40caefbb7355a1097efd2ede624f
Lukasz Malucha <lucasmalucha@gmail.com>
2025-06-22 13:21:09 +0100

    error handling

--- logs/src/main.rs
+++ logs/src/main.rs
@@ -1,48 +1,19 @@
 use std::fs;
 
-use std::io::Error;
-
 
 fn main() {
-    // let text = fs::read_to_string("logs.txt");
-
-    // println!("{:#?}", text);
-
-    match divide(5.0, 0.0) {
-        Ok(result_of_division) => {
-            println!("{}", result_of_division);
+    match fs::read_to_string("logs.txt") {
+        Ok(text_that_was_read) => {
+            println!("{}", text_that_was_read.len())
         }
-        Err(what_went_wrong) => {
-            println!("{}", what_went_wrong);
+        Err(why_this_failed) => {
+            println!("Failed to read file: {}", why_this_failed)
         }
-    }
-
-    match validate_email(String::from("asd@asdf.com")) {
-        Ok(..) => println!("email ok"),
-        Err(reason_this_failed_validation) => {
-            println!("{}",reason_this_failed_validation)
-        }
+        
     }
 
 }
 
-fn validate_email(email: String) -> Result<(), Error> {
-    if email.contains("@") {
-        Ok(())
-    } else {
-        Err(Error::other("emails must have an @"))
-    }
-
-}
-
-
-fn divide(a: f64, b: f64) -> Result<f64, Error> {
-    if b == 0.0 {
-        Err(Error::other("cant divide by 0"))
-    } else {
-        Ok(a/b)
-    }
-}
 
 
 
