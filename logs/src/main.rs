d30052d3059efbbd046c3d770f3be2b21bd9e8c2
Lukasz Malucha <lucasmalucha@gmail.com>
2025-06-26 11:41:33 +0100

    borrowing

--- logs/src/main.rs
+++ logs/src/main.rs
@@ -1,17 +1,36 @@
 use std::fs;
 
 
+fn extract_errors(text: &str) -> Vec<&str> {
+    let split_text = text.split("\n");
+
+    let mut results = vec![];
+
+    for line in split_text {
+        if line.starts_with("ERROR") {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 fn main() {
+    let mut error_logs = vec![];
+
 
     match fs::read_to_string("logs.txt") {
         Ok(text_that_was_read) => {
-            println!("{}", text_that_was_read.len())
+            error_logs = extract_errors(text_that_was_read.as_str());
+            
         }
         Err(why_this_failed) => {
             println!("Failed to read file: {}", why_this_failed)
         }
         
     }
+
+    println!("{:#?}", error_logs);
 
 }
 
