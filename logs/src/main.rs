ac047c7c218940f828b1fa72cf166d657d9bce9f
Lukasz Malucha <lucasmalucha@gmail.com>
2025-07-04 16:38:13 +0100

    rework

--- logs/src/main.rs
+++ logs/src/main.rs
@@ -17,26 +17,31 @@ fn extract_errors(text: &str) -> Vec<String> {
 
 fn main() {
 
+    let text = fs::read_to_string("logs.txt").expect("failed to read");
 
-    match fs::read_to_string("logs.txt") {
-        Ok(text_that_was_read) => {
-            let error_logs = extract_errors(text_that_was_read.as_str());
-            println!("{:#?}", error_logs);
+    let error_logs = extract_errors(text.as_str());
+    fs::write("errors.txt", error_logs.join("\n")).expect("failed to write errors.txt");
 
-            match fs::write("errors.txt", error_logs.join("\n")) {
-                Ok(..) => println!("File created"),
-                Err(reason_write_failed) => {
-                    println!("Failed to write file: {}", reason_write_failed)
-                }
 
-            }
+    // match fs::read_to_string("logs.txt") {
+    //     Ok(text_that_was_read) => {
+    //         let error_logs = extract_errors(text_that_was_read.as_str());
+    //         println!("{:#?}", error_logs);
+
+    //         match fs::write("errors.txt", error_logs.join("\n")) {
+    //             Ok(..) => println!("File created"),
+    //             Err(reason_write_failed) => {
+    //                 println!("Failed to write file: {}", reason_write_failed)
+    //             }
+
+    //         }
             
-        }
-        Err(why_this_failed) => {
-            println!("Failed to read file: {}", why_this_failed)
-        }
+    //     }
+    //     Err(why_this_failed) => {
+    //         println!("Failed to read file: {}", why_this_failed)
+    //     }
         
-    }
+    // }
 
 }
 
