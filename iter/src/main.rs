c6f9488ed2471ec482dada37cdd51aca2f62e76e
Lukasz Malucha <lucasmalucha@gmail.com>
2025-07-12 18:39:30 +0100

    for each

--- iter/src/main.rs
+++ iter/src/main.rs
@@ -1,10 +1,15 @@
 
-fn print_elements(elements: &Vec<String>) {
-   for element in elements {
-        println!("{}", element);
-   } 
+// fn print_elements(elements: &Vec<String>) {
+//    for element in elements {
+//         println!("{}", element);
+//    } 
 
+// }
+
+fn print_elements(elements: &Vec<String>) {
+    elements.iter().for_each(|el| println!("{}", el));
 }
+
 
 fn main() {
     let colors = vec![
