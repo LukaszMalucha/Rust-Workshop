bfc89630edfc55e6f9be5571a4e7941b35a863f1
Lukasz Malucha <lucasmalucha@gmail.com>
2025-07-21 18:13:06 +0100

    move elements

--- iter/src/main.rs
+++ iter/src/main.rs
@@ -1,4 +1,4 @@
-
+use std::collections::LinkedList;
 // fn print_elements(elements: &Vec<String>) {
 //    for element in elements {
 //         println!("{}", element);
@@ -19,8 +19,15 @@ fn shorten_strings(elements: &mut [String]) {
     elements.iter_mut().for_each(|el| el.truncate(1));
 }
 
+
+
 fn to_uppercase(elements: &[String]) -> Vec<String> {
-    elements.iter().map(|el| el.to_uppercase()).collect()
+    elements.iter().map(|el| el.to_uppercase()).collect::<Vec<_>>()
+}
+
+fn move_elements(vec_a: Vec<String>, vec_b: &mut Vec<String>) {
+    vec_a.into_iter().for_each(|el| vec_b.push(el))
+
 }
 
 
@@ -37,6 +44,10 @@ fn main() {
 
     // println!("{:#?}", colors);
 
-    let uppercase = to_uppercase(&colors);
-    println!("{:#?}", uppercase);
+    // let uppercase = to_uppercase(&colors);
+    // println!("{:#?}", uppercase);
+
+    let mut destination = vec![];
+    move_elements(colors, &mut destination);
+    println!("Destination: {:#?}", destination);
 }
